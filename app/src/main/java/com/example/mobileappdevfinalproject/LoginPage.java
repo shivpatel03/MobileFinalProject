f2af7cc6b0f9d2e1156b5ae56900bfd2743f6b81
Shiv Patel <da.shiv.patel@gmail.com>
2024-10-29 21:23:01 -0400

    Wrote simple main page

--- app/src/main/java/com/example/mobileappdevfinalproject/LoginPage.java
+++ app/src/main/java/com/example/mobileappdevfinalproject/LoginPage.java
@@ -1,6 +1,5 @@
 package com.example.mobileappdevfinalproject;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import android.view.View;
-import android.widget.*;
-
-public class MainActivity extends AppCompatActivity {
-
-    Button loginButton;
+public class LoginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        loginButton = findViewById(R.id.loginButton);
-    }
-
-    // redirect to login page onClick
-    public void redirectToLoginPage(View view) {
-        Intent intent = new Intent(MainActivity.this, LoginPage.class);
-        startActivity(intent);
     }
 }
